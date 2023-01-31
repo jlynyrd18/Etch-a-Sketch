088ba77644a3c0283266ee78c062cf67beee8ab9
Jordan Lenard <leonard_2010@live.com>
2023-01-30 22:03:05 -0600

    created container for div

---
+++ Etch.js
@@ -0,0 +1,14 @@
+//webpage must load with a 16x16 grid of square divs, put inside a container div
+const container = document.createElement("div");
+container.style.width = "600px";
+container.style.height = "400px";
+body.append(contianer);
+
+//fucntion for loop to run create element 64 times if no user input?
+
+
+//event listeners for hovering mouse, try class color of white, then when hovered change class to black
+
+
+//button top screen asking for grid size, once entered the existing grid should be removed and a new grid should be generated in same space
+//research button tags and prompts, 
