1e34f119951317e884b9ef25e0d3726ad3598ad2
Jordan Lenard <leonard_2010@live.com>
2023-01-31 21:54:09 -0600

    function doesn't make divs as needed will work on

--- Etch.js
+++ Etch.js
@@ -2,21 +2,30 @@
 const container = document.createElement("div");
 container.style.width = "600px";
 container.style.height = "400px";
-body.append(container);
+document.body.append(container);
+container.style.border = "3px solid black";
+container.style.flex = "flex";
+container.id = "container";
+
 
 //fucntion for loop to run create element 64 times if no user input?
 const size = 0;
-function etch_grid (size) {
+container.onload = function etchGrid (size) {
     if (size === 0) {
         for (let i = 0; i < 64; i++) {
             const gridDefault = document.createElement("div");
+            container.appendChild(gridDefault);
         }
-        container.appendChild(gridDefault);
+        
     }else if (size !== 0) {
         for (let i = 0; i < size; i++) {
             const gridInput = document.createElement("div");
+            container.appendChild(gridInput);
         }
-    }   container.appendChild(gridInput);
+    }   
+    
+    gridDefault.style.border = "1px solid black";
+    gridInput.style.border = "1px solid black";
 }
 
 //event listeners for hovering mouse, try class color of white, then when hovered change class to black
@@ -24,3 +33,4 @@ function etch_grid (size) {
 
 //button top screen asking for grid size, once entered the existing grid should be removed and a new grid should be generated in same space
 //research button tags and prompts, 
+const userInput = document.createElement("button");
