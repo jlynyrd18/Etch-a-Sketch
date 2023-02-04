9160205bf5683317793fb86c27b684e413773da0
Jordan Lenard <leonard_2010@live.com>
2023-02-03 23:45:17 -0600

    making box for size adjustment

--- Etch.js
+++ Etch.js
@@ -9,23 +9,25 @@ container.id = "container";
 
 
 //fucntion for loop to run create element 64 times if no user input?
-const size = 0;
-container.onload = function etchGrid (size) {
-    if (size === 0) {
-        for (let i = 0; i < 64; i++) {
+const size = 64;
+document.body.onload = etchGrid;
+function etchGrid (size) {
+    if (size === 64) {
+        for (let i = 0; i <= 64; i++) {
             const gridDefault = document.createElement("div");
+            gridDefault.style.border = "1px solid black";
+            gridDefault.className = "pix";
             container.appendChild(gridDefault);
         }
         
-    }else if (size !== 0) {
+    }else if (size !== 64) {
         for (let i = 0; i < size; i++) {
             const gridInput = document.createElement("div");
+            gridInput.style.border = "1px solid black";
+            gridInput.className = "pix";
             container.appendChild(gridInput);
         }
     }   
-    
-    gridDefault.style.border = "1px solid black";
-    gridInput.style.border = "1px solid black";
 }
 
 //event listeners for hovering mouse, try class color of white, then when hovered change class to black
@@ -33,4 +35,10 @@ container.onload = function etchGrid (size) {
 
 //button top screen asking for grid size, once entered the existing grid should be removed and a new grid should be generated in same space
 //research button tags and prompts, 
+const controlBox = document.createElement("div");
+controlBox.style.width = "100px";
+controlBox.style.height = "250px";
+document.body.append(controlBox);
+controlBox.style.border = "3px solid black";
+
 const userInput = document.createElement("button");
