06a120c3a6e611c1d6d9d9ae36385b28421056c6
Jordan Lenard <leonard_2010@live.com>
2023-02-08 21:33:08 -0600

    button fixed, need function and add evetlistener

--- Etch.js
+++ Etch.js
@@ -23,55 +23,46 @@ container.id = "container";
 //button top screen asking for grid size, once entered the existing grid should be removed and a new grid should be generated in same space
 //research button tags and prompts, 
 const controlBox = document.createElement("div");
-controlBox.style.width = "100px";
+controlBox.style.width = "200px";
 controlBox.style.height = "250px";
 container.append(controlBox);
 controlBox.style.border = "3px solid black";
 
 //inside box need text field saying description, input field with size of grid, and button to refresh and run function
 const desc = document.createElement("h3");
-desc.textContent = "Size";
+desc.textContent = "Size of board";
 controlBox.appendChild(desc);
 desc.style.textAlign = "center";
 
 const userInput = document.createElement("input");
 controlBox.appendChild(userInput);
 userInput.id = "userInput";
+userInput.setAttribute("type", "number");
+userInput.defaultValue = "64";
 //gets value of input field  size = document.getElementById("userInput").value; make requirement for value to be no more than 100
-
+const size = document.getElementById("userInput").value;
 
 const refreshBtn = document.createElement("button");
-controlBox.appendChild(refreshBtn);
 refreshBtn.id = "refreshBtn";
-refreshBtn.type = "submit";
+refreshBtn.textContent = "Refresh";
+controlBox.appendChild(refreshBtn);
+refreshBtn.addEventListener("click", etchGrid);
 
 //webpage must load with a 16x16 grid of square divs, put inside a container div
 const board = document.createElement("div");
 container.append(board);
 board.id = "board";
 
-
 //fucntion for loop to run create element 64 times if no user input?
-const size = 64;
-document.body.onload = etchGrid;
 function etchGrid (size) {
-    if (size === 64) {
-        for (let i = 0; i <= 64; i++) {
-            const gridDefault = document.createElement("div");
-            gridDefault.style.border = "1px solid black";
-            gridDefault.className = "pix";
-            document.getElementById("board").appendChild(gridDefault);
-        }
-        
-    }else if (size !== 64) {
-        for (let i = 0; i < size; i++) {
-            const gridInput = document.createElement("div");
-            gridInput.style.border = "1px solid black";
-            gridInput.className = "pix";
-            board.appendChild(gridInput);
-        }
-    }   
+    for(i = 0; i < size; i ++){
+        const pix = document.createElement("div");
+        pix.className = "pixColor";
+        board.appendChild(pix);
+    }
+
 }
+
 
 //event listeners for hovering mouse, try class color of white, then when hovered change class to black
 
